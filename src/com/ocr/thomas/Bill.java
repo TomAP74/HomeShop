da2e0c62ff0ef25111db86c6b9a62345e583e8dc
TomP74 <pons_thomas@orange.fr>
2019-02-17 18:16:08 +0100

    add class FileWriter. add test in BillTest. add behavior in Main.

--- src/com/ocr/thomas/Bill.java
+++ src/com/ocr/thomas/Bill.java
@@ -1,5 +1,6 @@
 package com.ocr.thomas;
 
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,4 +25,41 @@ public class Bill {
     public Map<Product, Integer> getProducts() {
         return products;
     }
+
+    public void generate(Writer writer){
+        writer.start();
+        writer.writeLine("HomeShop compagnie");
+        writer.writeLine("1 Place Charles de Gaulle, 75008 Paris");
+        writer.writeLine("");
+        writer.writeLine("Facture à l'attention de : ");
+        writer.writeLine(customer.getFullname());
+        writer.writeLine(customer.getAddress());
+        writer.writeLine("");
+        writer.writeLine("Mode de livraison : " + delivery.getInfo());
+        writer.writeLine("");
+        writer.writeLine("Produits : ");
+        writer.writeLine("-----------------------------------------------------");
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            writer.writeLine(product.getName() + " - " + product.getPrice() + " - " + quantity + " unité(s)");
+            writer.writeLine(product.getDescription());
+            writer.writeLine("");
+        }
+        writer.writeLine("Livraison : " + delivery.getPrice());
+        writer.writeLine("-----------------------------------------------------");
+        writer.writeLine("Total : " + this.getTotal());
+        writer.stop();
+    }
+
+    public double getTotal() {
+        double total = delivery.getPrice();
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            total += product.getPrice() * quantity;
+        }
+        return total;
+    }
+
 }
